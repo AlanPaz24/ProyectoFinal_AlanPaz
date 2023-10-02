ad5a615727df9ff313161357fc106e906682cd05
AlanPaz24 <alan.a.paz@live.com>
2023-10-01 21:01:37 -0300

    se crearon fetch

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("loan-form");
-    const totalElement = document.getElementById("total");
-    const cuotaElement = document.getElementById("cuota");
-    const paisSelect = document.getElementById("pais");
+$(document).ready(function () {
+    const form = $("#loan-form");
+    const totalElement = $("#total");
+    const cuotaElement = $("#cuota");
+    const paisSelect = $("#pais");
 
     // Cargar datos de países desde el archivo JSON
-    fetch("paises.json")
-        .then((response) => response.json())
-        .then((data) => {
-            data.forEach((pais) => {
-                const option = document.createElement("option");
-                option.value = pais.nombre;
-                option.textContent = pais.nombre;
-                paisSelect.appendChild(option);
-            });
+    $.getJSON("paises.json", function (data) {
+        $.each(data, function (key, pais) {
+            paisSelect.append($("<option>", {
+                value: pais.nombre,
+                text: pais.nombre
+            }));
         });
+    });
 
-    form.addEventListener("submit", (e) => {
+    form.submit(function (e) {
         e.preventDefault();
 
-        const nombre = document.getElementById("nombre").value;
-        const apellido = document.getElementById("apellido").value;
-        const edad = document.getElementById("edad").value;
-        const monto = parseFloat(document.getElementById("monto").value);
-        const cuotas = parseInt(document.getElementById("cuotas").value);
+        const nombre = $("#nombre").val();
+        const apellido = $("#apellido").val();
+        const edad = $("#edad").val();
+        const monto = parseFloat($("#monto").val());
+        const cuotas = parseInt($("#cuotas").val());
 
         // Calcular el total a pagar con un 80% de interés anual
         const tasaInteresAnual = 0.8;
         const tasaInteresMensual = tasaInteresAnual / 12;
-        const totalAPagar = monto * (1 + tasaInteresMensual) ** cuotas;
+        const totalAPagar = monto * Math.pow(1 + tasaInteresMensual, cuotas);
 
         // Calcular el valor de la cuota mensual
         const valorCuota = totalAPagar / cuotas;
 
-        totalElement.textContent = `$${totalAPagar.toFixed(2)}`;
-        cuotaElement.textContent = `$${valorCuota.toFixed(2)}`;
+        totalElement.text(`$${totalAPagar.toFixed(2)}`);
+        cuotaElement.text(`$${valorCuota.toFixed(2)}`);
 
-        document.getElementById("resultado").style.display = "block";
+        $("#resultado").show();
     });
 });
